ed06a76b1035afced814bce6bcea3a260854af0c
Thomas Fisher <tommyfisher@gmail.com>
2013-02-12 20:58:51 -0800

    initial commit

---
+++ poker.rb
@@ -0,0 +1,208 @@
+class Card
+  attr_accessor :suit, :rank
+
+  RANKS = {
+    "A" => 14,
+    "K" => 13,
+    "Q" => 12,
+    "J" => 11,
+    "T" => 10
+  }
+
+  def initialize(str)
+    rank, suit = str.split("")
+
+    @suit = suit
+    @rank = RANKS[rank] || rank.to_i
+  end
+
+  def <=>(card)
+    card.rank <=> rank
+  end
+
+  def as_low_ace
+    if rank == 14
+      c = self.dup
+      c.rank = 1
+      c
+    else
+      self
+    end
+  end
+
+  def to_s
+    (RANKS.invert[rank] || rank.to_s) + suit
+  end
+end
+
+class Hand
+  attr_reader :cards
+
+  RANKINGS = [:royal_flush, :straight_flush, :four_of_a_kind,
+             :full_house, :flush, :straight, :three_of_a_kind,
+             :two_pair, :one_pair, :high_card]
+
+  def initialize(str = "")
+    @cards = str.split(" ").map { |s| Card.new(s) }
+  end
+
+  def royal_flush?
+    flush? && straight? && kickers.first.rank == 14
+  end
+
+  def straight_flush?
+    flush? && straight?
+  end
+
+  def flush?
+    cards.map(&:suit).uniq.length == 1
+  end
+
+  def straight?
+    high_ace_straight? || low_ace_straight?
+  end
+
+  def four_of_a_kind?
+    num_groups == 1 && group_lengths == [4]
+  end
+
+  def full_house?
+    num_groups == 2 && group_lengths.sort == [2, 3]
+  end
+
+  def three_of_a_kind?
+    num_groups == 1 && group_lengths == [3]
+  end
+
+  def two_pair?
+    num_groups == 2 && group_lengths == [2,2]
+  end
+
+  def one_pair?
+    num_groups == 1 && group_lengths == [2]
+  end
+
+  def high_card?
+    true
+  end
+
+  def kickers
+    cards.sort
+  end
+
+  def <=>(hand)
+    # lower indexes are better, so the comparison is swapped
+    comparison = RANKINGS.index(hand.rank) <=> RANKINGS.index(self.rank)
+    comparison = hand.kickers <=> self.kickers if comparison == 0
+    comparison
+  end
+
+  def >(hand)
+    (self <=> hand) == 1
+  end
+
+  def <(hand)
+    (self <=> hand) == -1
+  end
+
+  def ==(hand)
+    (self <=> hand) == 0
+  end
+
+  def rank
+    RANKINGS.detect do |rank|
+      self.send("#{rank}?")
+    end
+  end
+
+  def to_s
+    cards.sort.map(&:to_s).join(" ")
+  end
+
+  private
+
+  def high_ace_straight?
+    cards.sort.map(&:rank).each_cons(2).all? { |first, second| first - second == 1 }
+  end
+
+  def low_ace_straight?
+    cards.map(&:as_low_ace).sort.map(&:rank).each_cons(2).all? { |first, second| first - second == 1 }
+  end
+
+  def num_groups
+    rank_grouped.length
+  end
+
+  def group_lengths
+    rank_grouped.map(&:length)
+  end
+
+  def rank_grouped
+    cards.group_by(&:rank).reject { |rank, cards| cards.length == 1 }.values
+  end
+end
+
+class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    %w{h d c s}.each do |suit|
+      %w{2 3 4 5 6 7 8 9 T J Q K A}.each do |rank|
+        @cards << Card.new(rank + suit)
+      end
+    end
+    shuffle!
+  end
+
+  def shuffle!
+    cards.shuffle!
+  end
+
+  def deal!
+    cards.pop
+  end
+end
+
+class Poker
+  class << self
+
+    def play!(players = 2)
+      raise "Can't deal to more than 10 players!" if players > 10
+      raise "Need at least two players!" if players < 2
+      deck = Deck.new
+
+      hands = players.times.map { Hand.new }
+
+      5.times do
+        hands.each do |hand|
+          hand.cards << deck.deal!
+        end
+      end
+
+      hands.sort!.reverse!
+
+      winners = [hands.shift]
+      losers = []
+
+      hands.each do |hand|
+        if winners.first == hand
+          winners << hand
+        else
+          losers << hand
+        end
+      end
+
+      winners.each do |hand|
+        puts "#{hand} - #{hand.rank} (winner)"
+      end
+      losers.each do |hand|
+        puts "#{hand} - #{hand.rank} (loser)"
+      end
+
+      winners
+    end
+
+  end
+end
+
---
+++ poker_spec.rb
@@ -0,0 +1,121 @@
+require './poker'
+require 'rspec'
+
+# There's a bit more metaprogramming in this spec than is typical to my tastes
+# but I thought it made the specs more concise
+
+describe Hand do
+  subject { Hand.new(cards) }
+
+  RANKED_HANDS = {
+    :royal_flush => "Th Qh Jh Kh Ah",
+    :straight_flush => "Th 9h Jh Qh 8h",
+    :flush => "Th 9h Jh Qh 7h",
+    :straight => "Jh Td Qs Ks 9h",
+    :four_of_a_kind => "Th Ts Td Tc 8h",
+    :full_house => "Th Ts Tc Qh Qd",
+    :three_of_a_kind => "Th Ts Td 7c 8h",
+    :two_pair => "Th Ts Jd 8c 8h",
+    :one_pair => "Th Ts Jd 6c 8h"
+  }
+
+  RANKED_HANDS.each do |name, cards|
+    let(name) { Hand.new(cards) }
+  end
+
+  describe "royal flushes" do
+    subject { royal_flush }
+
+    it { should be_royal_flush }
+    it { should be_straight_flush }
+    it { should be_straight }
+    it { should be_flush }
+    it { should_not be_four_of_a_kind }
+    it { should_not be_full_house }
+    it { should_not be_three_of_a_kind }
+    it { should_not be_two_pair }
+    it { should_not be_one_pair }
+
+    its(:rank) { should == :royal_flush }
+
+    [:straight_flush, :flush, :straight, :four_of_a_kind, :full_house, :three_of_a_kind, :two_pair, :one_pair].each do |hand|
+      it "beat a #{hand}" do
+        subject.should be > send(hand)
+      end
+    end
+  end
+
+  describe "straight flushes" do
+    subject { straight_flush }
+
+    it { should_not be_royal_flush }
+    it { should be_straight_flush }
+    it { should be_straight }
+    it { should be_flush }
+    it { should_not be_four_of_a_kind }
+    it { should_not be_full_house }
+    it { should_not be_three_of_a_kind }
+    it { should_not be_two_pair }
+    it { should_not be_one_pair }
+
+    its(:rank) { should == :straight_flush }
+
+    it "loses to a royal flush" do
+      subject.should be < royal_flush
+    end
+    [:flush, :straight, :four_of_a_kind, :full_house, :three_of_a_kind, :two_pair, :one_pair].each do |hand|
+      it "beat a #{hand}" do
+        subject.should be > send(hand)
+      end
+    end
+  end
+
+  other_hands = [:four_of_a_kind, :full_house, :flush, :straight, :three_of_a_kind, :two_pair, :one_pair]
+
+  other_hands.each do |hand|
+    describe hand.to_s do
+      subject { send(hand) }
+
+      RANKED_HANDS.each do |hand_name, cards|
+        if hand == hand_name
+          it { should send("be_#{hand_name}") }
+        else
+          it { should_not send("be_#{hand_name}") }
+        end
+      end
+
+      it "loses to a royal flush" do
+        subject.should be < royal_flush
+      end
+
+      it "loses to a straight flush" do
+        subject.should be < straight_flush
+      end
+
+      hand_index = other_hands.index(hand)
+
+      other_hands.slice(0, hand_index).each do |winning_hand|
+        it "loses to a #{winning_hand}" do
+          subject.should be < send(winning_hand)
+        end
+      end
+
+      other_hands.slice(hand_index + 1, other_hands.length - hand_index).each do |losing_hand|
+        it "beats a #{losing_hand}" do
+          subject.should be > send(losing_hand)
+        end
+      end
+    end
+  end
+
+  # there'd be more tests here, for example:
+
+  describe "straights with ace low" do
+    subject { Hand.new("Ah 3c 4d 5s 2c") }
+
+    it { should be_straight }
+    its(:rank) { should == :straight }
+  end
+
+  # I'd also be testing high card hands, kicker comparison, etc.
+end
